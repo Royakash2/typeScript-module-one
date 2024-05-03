e4d7a58aa0a5ebd667147695d60b16c743bd657e
Royakash2 <yahmeakash@gmail.com>
2024-05-03 15:06:01 +0600

    type alias and interface

---
+++ Module-2/2.2.ts
@@ -0,0 +1,32 @@
+{
+
+    // interface
+    // note: shokol primitive data ar khetre amra type alias use korbo r object ar khetre interface bebohar korbo abr type alias o use korte parbo 
+    // 2. type alias a jude amra ekta property barate chai taile amra inter section use korte pari (&), interface ar khetre amra (extends) use korte type barate pari ;
+
+    type userOne = {
+        name: string;
+        age: number;
+    };
+    interface user2 {
+        name: string;
+        age: number;
+    }
+    type rollUser = userOne & {role: number}; /* type alias intersection (&) use kore property bariyechi*/
+    interface rollUser2 extends user2{
+        role: number;
+    } /* interface a  (extends) use kore property bariyechi*/
+    
+    // type alias dara array declear 
+    type Roll = number[];
+    interface Roll2 {
+        [index: number] : number;
+    }
+    const rollNumber : Roll2 = [1,2,3,4,5,6,7]
+    const user1: rollUser = {
+        name: 'Akash',
+        age: 20,
+        role: 34,
+    }
+    console.log(user1);
+}
